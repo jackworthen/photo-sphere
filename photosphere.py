bad604742796583925cc643ca0ed0dfd83d1c533
jackworthen <jworthen82@gmail.com>
2025-09-04 07:15:30 -0400

    Update photosphere.py
    further adjusted window size

--- photosphere.py
+++ photosphere.py
@@ -2093,7 +2093,7 @@ class PhotoSphereMainWindow(QMainWindow):
     
     def setup_ui(self):
         self.setWindowTitle("PhotoSphere")
-        self.setGeometry(100, 100, 1250, 900)
+        self.setGeometry(100, 100, 1290, 900)
         
         # Set application icon
         try:
